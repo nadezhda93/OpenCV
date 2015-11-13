1208cfb224abe3a441906ad8233c99083c307e90
Nadezhda Shivarova <nadia93.ie@gmail.com>
2015-11-13 14:57:00 +0000

    Simple video capture from webcam, convert video to greyscale.

---
+++ VideoTut.py
@@ -0,0 +1,25 @@
+import numpy as np
+import cv2
+
+# create an object for the video, the argument is the number of the 
+# camera, in this case only one. Replace with path or file 
+# name to play from file
+cap = cv2.VideoCapture(0) 
+
+while(True):
+	#Capture frame by frame from object
+	# returns a boolean TRUE if frame is read correctly
+	ret, frame = cap.read()
+
+	#Operations on a frame - convert to greyscale
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+	# Show the resulting frame
+	cv2.imshow('frame',gray)
+
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+	#When everything is done, release the capture
+	cap.release()
+	cv.destroyAllWindows()
